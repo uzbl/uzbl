2cf22958352bdffbc068413fa42b8df53ae3e41a
Dieter Plaetinck <dieter@plaetinck.be>
2009-04-25 10:14:01 +0200

    simple bench scripts for dmenu/awk/sort/..

---
+++ misc/dmenu-performancetest-generate-dummy-history-file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Creating dummy file of 50MB in size (625000 entries of 80chars)"
+entries_per_iteration=1000
+for i in `seq 1 625`
+do
+	echo "Iteration $i of 625 ( $entries_per_iteration each )"
+	for j in `seq 1 $entries_per_iteration`
+	do
+		echo "`date +'%Y-%m-%d %H:%M:%S'` `date +%s`abcdefhijklmno`date +%s | md5sum`" >> ./dummy_history_file
+	done
+done
