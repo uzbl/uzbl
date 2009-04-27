0e6673e461793f8c317db0f57bd49806d4e5457d
Dieter Plaetinck <dieter@plaetinck.be>
2009-04-27 21:26:42 +0200

    remove files, should have happened during previous merge but whatever

--- extra/load_url_from_bookmarks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-file=bookmarks
-goto=`awk '{print $1}' $history_file | dmenu` #NOTE: it's the job of the script that inserts bookmarks to make sure there are no dupes.
-[ -n "$goto" ] && echo "uri $goto" > $4
