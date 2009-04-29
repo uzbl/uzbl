3893863e204466b73939fcbeae1a714679a04aad
Dieter Plaetinck <dieter@plaetinck.be>
2009-04-29 19:51:15 +0200

    dmenu -i in example scripts

--- examples/scripts/load_url_from_history.sh
+++ examples/scripts/load_url_from_history.sh
@@ -2,5 +2,5 @@
 # you probably really want this in your $XDG_DATA_HOME (eg $HOME/.local/share/uzbl/history)
 history_file=/tmp/uzbl.history
 
-goto=`awk '{print $3}' $history_file | sort | uniq | dmenu`
+goto=`awk '{print $3}' $history_file | sort | uniq | dmenu -i`
 [ -n "$goto" ] && echo "uri $goto" > $4
