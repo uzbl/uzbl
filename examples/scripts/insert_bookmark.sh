8776679111e15ae5522d42a5f92993a57d71832c
Dieter Plaetinck <dieter@plaetinck.be>
2009-05-04 20:04:05 +0200

    bookmarks location update + slight fixes re INSTALL file

--- examples/scripts/insert_bookmark.sh
+++ examples/scripts/insert_bookmark.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # you probably want your bookmarks file in your $XDG_DATA_HOME ( eg $HOME/.local/share/uzbl/bookmarks)
 
-if [ -f /usr/share/uzbl/examples/bookmarks ]
+if [ -f /usr/share/uzbl/examples/data/bookmarks ]
 then
-	file=/usr/share/uzbl/examples/bookmarks # you will probably get permission denied errors here. pick a file in your ~
+	file=/usr/share/uzbl/examples/data/bookmarks # you will probably get permission denied errors here. pick a file in your ~
 else
 	file=./examples/bookmarks #useful when developing
 fi
--- examples/scripts/load_url_from_bookmarks.sh
+++ examples/scripts/load_url_from_bookmarks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # you probably want your bookmarks file in your $XDG_DATA_HOME ( eg $HOME/.local/share/uzbl/bookmarks)
-if [ -f /usr/share/uzbl/examples/bookmarks ]
+if [ -f /usr/share/uzbl/examples/data/bookmarks ]
 then
-        file=/usr/share/uzbl/examples/bookmarks
+        file=/usr/share/uzbl/examples/data/bookmarks
 else
         file=./examples/bookmarks #useful when developing
 fi
