a759f9ca068fa219d171fcc9d79a4a063818de2d
Dieter Plaetinck <dieter@plaetinck.be>
2009-05-16 09:23:21 +0200

    support dmenu vertical patch (with gracefull fallback to normal dmenu)

--- examples/scripts/load_url_from_history.sh
+++ examples/scripts/load_url_from_history.sh
@@ -2,10 +2,17 @@
 # you probably really want this in your $XDG_DATA_HOME (eg $HOME/.local/share/uzbl/history)
 history_file=/tmp/uzbl.history
 
+if dmenu --help 2>&1 | grep -q '\[-rs\] \[-ni\] \[-nl\] \[-xs\]'
+then
+        DMENU="dmenu -i -xs -rs -l 10" # vertical patch
+else    
+        DMENU="dmenu -i"
+fi 
+
 # choose from all entries, sorted and uniqued
 # goto=`awk '{print $3}' $history_file | sort -u | dmenu -i`
 
 # choose from all entries, the first one being current url, and after that all others, sorted and uniqued.
-current=`tail -n 1 $history_file | awk '{print $3}'`; goto=`(echo $current; awk '{print $3}' $history_file | grep -v "^$current\$" | sort -u) | dmenu -i`
+current=`tail -n 1 $history_file | awk '{print $3}'`; goto=`(echo $current; awk '{print $3}' $history_file | grep -v "^$current\$" | sort -u) | $DMENU`
 #[ -n "$goto" ] && echo "uri $goto" > $4
 [ -n "$goto" ] && uzblctrl -s $5 -c "uri $goto"
