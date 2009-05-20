1f8f2ae900a1b3b98d3438eb927782e7968a1ebf
Jan Kolkmeier <j.kolkmeier@student.utwente.nl>
2009-05-20 21:18:46 +0200

    fixed the download script (+ improvements?)

--- examples/scripts/download.sh
+++ examples/scripts/download.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-wget $6
+# A bit more advanced, try some pattern matching on the link to determine
+# a target location. pushd and popd might be better than plain cd...
+if [[ $8 =~ .*(.torrent) ]] 
+then
+    pushd /your/place/for/torrent/files
+else
+    pushd /your/place/for/usual/downloads
+fi
+# Some sites block the default wget --user-agent...
+wget --user-agent=Firefox $8
+popd
