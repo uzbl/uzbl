3099270345246d8dd726d41514af5c0e206a6196
uranther <jwheaton@purdue.edu>
2009-06-03 23:55:45 -0400

    doh! i don't need that strdup there

--- uzbl.c
+++ uzbl.c
@@ -2037,7 +2037,7 @@ create_mainbar () {
 static
 GtkWidget* create_window () {
     GtkWidget* window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gchar* uzbl_icon = g_strdup(find_xdg_file(1, "/uzbl/uzbl.png"));
+    gchar* uzbl_icon = find_xdg_file(1, "/uzbl/uzbl.png");
     gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
     gtk_widget_set_name (window, "Uzbl browser");
     gtk_window_set_icon_from_file (GTK_WINDOW (window), uzbl_icon, NULL);
