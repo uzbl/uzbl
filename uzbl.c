17af1bff36bb5cbf33ef63593358ba1f85a19515
uranther <jwheaton@purdue.edu>
2009-06-10 18:41:05 -0400

    Revert "cleanup when uzbl quits"
    This reverts commit 7d3e27bd39e7f20175c1aac4bdf9f0c2c4bafe72.

--- uzbl.c
+++ uzbl.c
@@ -577,7 +577,6 @@ destroy_cb (GtkWidget* widget, gpointer data) {
     (void) widget;
     (void) data;
     gtk_main_quit ();
-    clean_up();
 }
 
 static void
@@ -893,7 +892,6 @@ close_uzbl (WebKitWebView *page, GArray *argv) {
     (void)page;
     (void)argv;
     gtk_main_quit ();
-    clean_up();
 }
 
 /* --Statusbar functions-- */
