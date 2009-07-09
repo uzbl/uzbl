4be1386eeb4858bce3ade065deb2f351b9cae2ff
Robert Manea <gotmor@gmail.com>
2009-07-09 11:44:51 +0200

    free uzbl.gui.geometry before resetting it

--- uzbl.c
+++ uzbl.c
@@ -2715,6 +2715,9 @@ retreive_geometry() {
     gtk_window_get_position(GTK_WINDOW(uzbl.gui.main_window), &x, &y);
 
     g_string_printf(buf, "%dx%d+%d+%d", w, h, x, y);
+
+    if(uzbl.gui.geometry)
+        g_free(uzbl.gui.geometry);
     uzbl.gui.geometry = g_string_free(buf, FALSE);
 }
 
