b1f012aa38fd0238a2e88d6a96ccc23986dd2d96
Mason Larobina <mason.larobina@gmail.com>
2009-08-09 01:18:24 +0800

    Cleaned up trailing whitespace.

--- examples/data/uzbl/scripts/cookie_daemon.py
+++ examples/data/uzbl/scripts/cookie_daemon.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# For configuration and cookie daemon usage examples check out the the 
+# For configuration and cookie daemon usage examples check out the the
 # cookie daemon wiki page at http://www.uzbl.org/wiki/cookie_daemon.py
 #
 # Issues:
