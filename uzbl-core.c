0e2392c6d9656722996f3589ea30e013ca14d9f2
Robert Manea <gotmor@gmail.com>
2009-09-14 10:59:35 +0200

    free ressources

--- uzbl-core.c
+++ uzbl-core.c
@@ -585,14 +585,18 @@ setup_signal(int signr, sigfunc *shandler) {
 void
 clean_up(void) {
     send_event(INSTANCE_EXIT, uzbl.info.pid_str, NULL);
+    g_free(uzbl.info.pid_str);
+
+    g_free(uzbl.state.executable_path);
+    g_hash_table_destroy(uzbl.behave.commands);
+
+    if(uzbl.state.event_buffer)
+        g_ptr_array_free(uzbl.state.event_buffer, TRUE);
 
     if (uzbl.behave.fifo_dir)
         unlink (uzbl.comm.fifo_path);
     if (uzbl.behave.socket_dir)
         unlink (uzbl.comm.socket_path);
-
-    g_free(uzbl.state.executable_path);
-    g_hash_table_destroy(uzbl.behave.commands);
 }
 
 /* --- SIGNAL HANDLER --- */
