d2d73ad463f3d9f1c673d37457af159947b3faac
Robert Manea <gotmor@gmail.com>
2009-09-18 21:59:32 +0200

    fix print bug

--- uzbl.c
+++ uzbl.c
@@ -912,8 +912,11 @@ update_gui(WebKitWebView *page, GArray *argv, GString *result) {
 void
 print(WebKitWebView *page, GArray *argv, GString *result) {
     (void) page; (void) result;
+    gchar* buf;
 
-    g_string_assign(result, argv_idx(argv, 0));
+    buf = expand(argv_idx(argv, 0), 0);
+    g_string_assign(result, buf);
+    g_free(buf);
 }
 
 void
