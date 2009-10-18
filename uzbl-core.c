d1e717013bda33073d86dfff039c742165046d29
keis <keijser@gmail.com>
2009-10-18 22:08:38 +0200

    stop using deprecated gtk_widget_ref.

--- uzbl-core.c
+++ uzbl-core.c
@@ -1297,8 +1297,8 @@ move_statusbar() {
             !uzbl.gui.mainbar)
         return;
 
-    gtk_widget_ref(uzbl.gui.scrolled_win);
-    gtk_widget_ref(uzbl.gui.mainbar);
+    g_object_ref(uzbl.gui.scrolled_win);
+    g_object_ref(uzbl.gui.mainbar);
     gtk_container_remove(GTK_CONTAINER(uzbl.gui.vbox), uzbl.gui.scrolled_win);
     gtk_container_remove(GTK_CONTAINER(uzbl.gui.vbox), uzbl.gui.mainbar);
 
@@ -1310,8 +1310,8 @@ move_statusbar() {
         gtk_box_pack_start (GTK_BOX (uzbl.gui.vbox), uzbl.gui.scrolled_win, TRUE, TRUE, 0);
         gtk_box_pack_start (GTK_BOX (uzbl.gui.vbox), uzbl.gui.mainbar, FALSE, TRUE, 0);
     }
-    gtk_widget_unref(uzbl.gui.scrolled_win);
-    gtk_widget_unref(uzbl.gui.mainbar);
+    g_object_unref(uzbl.gui.scrolled_win);
+    g_object_unref(uzbl.gui.mainbar);
     gtk_widget_grab_focus (GTK_WIDGET (uzbl.gui.web_view));
     return;
 }
