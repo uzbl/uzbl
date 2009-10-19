c8d71fa5538003a48d367fea5a1d8b463b98ede3
keis <keijser@gmail.com>
2009-10-19 02:13:15 +0200

    Add simple commandline history plugin to EM.

---
+++ examples/data/uzbl/plugins/history.py
@@ -0,0 +1,124 @@
+__export__ = ['get_history']
+UZBLS = {}
+
+import random
+
+class History(object):
+    def __init__(self):
+        self._commands = []
+        self.cursor = None
+        self.__temp_tail = False
+        self.search_key = None
+
+    def prev(self):
+        if self.cursor is None:
+            self.cursor = len(self._commands) - 1
+        else:
+            self.cursor -= 1
+
+        if self.search_key:
+            while self.cursor >= 0 and self.search_key not in self._commands[self.cursor]:
+                self.cursor -= 1
+
+        if self.cursor < 0 or len(self._commands) == 0:
+            self.cursor = -1
+            return random.choice(end_messages)
+
+        return self._commands[self.cursor]
+
+    def next(self):
+        if self.cursor is None:
+            return ''
+
+        self.cursor += 1
+
+        if self.search_key:
+            while self.cursor < len(self._commands) and self.search_key not in self._commands[self.cursor]:
+                self.cursor += 1
+
+        if self.cursor >= len(self._commands) - (1 if self.__temp_tail else 0):
+            self.cursor = None
+            self.search_key = None
+
+            if self.__temp_tail:
+                print 'popping temporary'
+                self.__temp_tail = False
+                return self._commands.pop()
+            return ''
+
+        return self._commands[self.cursor]
+
+    def search(self, key):
+        self.search_key = key
+        return self.prev()
+
+    def add(self, cmd):
+        if self.__temp_tail:
+            self._commands.pop()
+            self.__temp_tail = False
+
+        self._commands.append(cmd)
+        self.cursor = None
+        self.search_key = None
+
+    def add_temporary(self, cmd):
+        assert not self.__temp_tail
+
+        self._commands.append(cmd)
+        self.__temp_tail = True
+        self.cursor = len(self._commands) - 1
+
+        print 'adding temporary', self
+
+    def __str__(self):
+        return "(History %s, %s)" % (self.cursor, str(self._commands))
+
+def get_history(uzbl):
+    return UZBLS[uzbl]
+
+def add_instance(uzbl, *args):
+    UZBLS[uzbl] = History()
+
+def del_instance(uzbl, *args):
+    if uzbl in UZBLS:
+        del UZBLS[uzbl]
+
+def keycmd_exec(uzbl, keylet):
+    history = get_history(uzbl)
+    cmd = keylet.key_cmd()
+    if cmd:
+        history.add(cmd)
+
+def history_prev(uzbl, _x):
+    history = get_history(uzbl)
+    cmd = uzbl.get_keylet().key_cmd()
+    if history.cursor is None and cmd:
+        history.add_temporary(cmd)
+
+    uzbl.set_keycmd(history.prev())
+    print 'PREV', history
+
+def history_next(uzbl, _x):
+    history = get_history(uzbl)
+    cmd = uzbl.get_keylet().key_cmd()
+
+    uzbl.set_keycmd(history.next())
+    print 'NEXT', history
+
+def history_search(uzbl, key):
+    history = get_history(uzbl)
+    uzbl.set_keycmd(history.search(key))
+    print 'SEARCH', history
+
+end_messages = ('Look behind you, A three-headed monkey!', 'error #4: static from nylon underwear.', 'error #5: static from plastic slide rules.', 'error #6: global warming.', 'error #9: doppler effect.', 'error #16: somebody was calculating pi on the server.', 'error #19: floating point processor overflow.', 'error #21: POSIX compliance problem.', 'error #25: Decreasing electron flux.', 'error #26: first Saturday after first full moon in Winter.', 'error #64: CPU needs recalibration.', 'error #116: the real ttys became pseudo ttys and vice-versa.', 'error #229: wrong polarity of neutron flow.', 'error #330: quantum decoherence.', 'error #388: Bad user karma.', 'error #407: Route flapping at the NAP.', 'error #435: Internet shut down due to maintenance.')
+
+def init(uzbl):
+    connects = {'INSTANCE_START': add_instance,
+        'INSTANCE_EXIT': del_instance,
+        'KEYCMD_EXEC': keycmd_exec,
+        'HISTORY_PREV': history_prev,
+        'HISTORY_NEXT': history_next,
+        'HISTORY_SEARCH': history_search
+    }
+
+    uzbl.connect_dict(connects)
