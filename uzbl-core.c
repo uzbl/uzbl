054c132b3a61ae5fc2fd4200392332c22d1180dd
Robert Manea <gotmor@gmail.com>
2009-10-21 14:42:14 +0200

    set wm class

--- uzbl-core.c
+++ uzbl-core.c
@@ -1946,8 +1946,11 @@ create_mainbar () {
 GtkWidget*
 create_window () {
     GtkWidget* window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
     gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
+    gtk_window_set_wmclass(GTK_WINDOW(window), "uzbl", "uzbl");
     gtk_widget_set_name (window, "Uzbl browser");
+
     g_signal_connect (G_OBJECT (window), "destroy",         G_CALLBACK (destroy_cb),         NULL);
     g_signal_connect (G_OBJECT (window), "configure-event", G_CALLBACK (configure_event_cb), NULL);
 
