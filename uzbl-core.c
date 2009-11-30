78fd079c9f69708c9809629dcb83565064aba7af
Rob <rob.manea@gmail.com>
2009-11-30 11:11:49 +0100

    remove default WM_CLASS setting, use --class instead

--- uzbl-core.c
+++ uzbl-core.c
@@ -2029,7 +2029,6 @@ create_window () {
     GtkWidget* window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
     gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
-    gtk_window_set_wmclass(GTK_WINDOW(window), "uzbl", "uzbl");
     gtk_widget_set_name (window, "Uzbl browser");
 
     g_signal_connect (G_OBJECT (window), "destroy",         G_CALLBACK (destroy_cb),         NULL);
