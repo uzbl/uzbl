a9de45fd575cb47914b71ac23544a797b276cf3c
Rob <rob.manea@gmail.com>
2009-12-04 09:25:03 +0100

    generate a message on SIGSEGV

--- uzbl-core.c
+++ uzbl-core.c
@@ -535,12 +535,16 @@ void
 catch_signal(int s) {
     if(s == SIGTERM ||
        s == SIGINT  ||
-       s == SIGSEGV ||
        s == SIGILL  ||
        s == SIGFPE  ||
        s == SIGQUIT) {
         clean_up();
         exit(EXIT_SUCCESS);
+    }
+    else if(s == SIGSEGV) {
+        clean_up();
+        fprintf(stderr, "Program aborted, segmentation fault!\nAttempting to clean up...\n");
+        exit(EXIT_FAILURE);
     }
     else if(s == SIGALRM && uzbl.state.event_buffer) {
         g_ptr_array_free(uzbl.state.event_buffer, TRUE);
