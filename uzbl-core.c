c419594e8aff0fdc17c21e9313169e08f3050a73
Dieter Plaetinck <dieter@plaetinck.be>
2009-12-06 18:46:57 +0100

    support executing commands both through shell and directly

--- uzbl-core.c
+++ uzbl-core.c
@@ -266,17 +266,24 @@ expand(const char *s, guint recurse) {
                 }
                 else if(recurse != 1 &&
                         etype == EXP_EXPR) {
-
-                    mycmd = expand(ret, 1);
-                    gchar *quoted = g_shell_quote(mycmd);
-                    gchar *tmp = g_strdup_printf("%s %s", 
-                            uzbl.behave.shell_cmd?uzbl.behave.shell_cmd:"/bin/sh -c", 
-                            quoted);
-                    g_spawn_command_line_sync(tmp, &cmd_stdout, NULL, NULL, &err);
-                    g_free(mycmd);
-                    g_free(quoted);
-                    g_free(tmp);
-
+                    /* execute program directly */
+                    if(ret[0] == '+') {
+                        mycmd = expand(ret+1, 1);
+                        g_spawn_command_line_sync(mycmd, &cmd_stdout, NULL, NULL, &err);
+                        g_free(mycmd);
+                    }
+                    /* execute program through shell, quote it first */
+                    else {
+                        mycmd = expand(ret, 1);
+                        gchar *quoted = g_shell_quote(mycmd);
+                        gchar *tmp = g_strdup_printf("%s %s",
+                                uzbl.behave.shell_cmd?uzbl.behave.shell_cmd:"/bin/sh -c",
+                                quoted);
+                        g_spawn_command_line_sync(tmp, &cmd_stdout, NULL, NULL, &err);
+                        g_free(mycmd);
+                        g_free(quoted);
+                        g_free(tmp);
+                    }
                     if (err) {
                         g_printerr("error on running command: %s\n", err->message);
                         g_error_free (err);
