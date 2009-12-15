45483f33990f59da6eeaf31ca791cd0980911fc2
Pawel Tomak <satherot@gmail.com>
2009-12-15 13:41:48 +0100

    Note of the origins of the script

--- eFormFiller.sh
+++ eFormFiller.sh
@@ -12,6 +12,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+#   Based on script from Uzbl examples (anonymous author)
 #   Copyright (c) 2009 by Paweł Tomak <satherot (at) gmail (dot) com>
 #
 #
