uzbl/uzbl

commit 497a01b283266047d3496db14571be600a8d732d

kill gnuizm
sed -i is gnuism and it doesn't work on MacOS X and possibly other UNIX
systems. Reported by __monty__ on IRC, thanks.

Makefile
@@ -96,15 +96,21 @@ install-uzbl-core: all
 	install -m644 AUTHORS $(DOCDIR)/
 	cp -r examples $(INSTALLDIR)/share/uzbl/
 	chmod 755 $(INSTALLDIR)/share/uzbl/examples/data/scripts/*
-	sed -i 's#^set prefix.*=.*#set prefix     = $(RUN_PREFIX)#' $(INSTALLDIR)/share/uzbl/examples/config/config
+	mv $(INSTALLDIR)/share/uzbl/examples/config/config{,.bak}
+	sed 's#^set prefix.*=.*#set prefix     = $(RUN_PREFIX)#' < $(INSTALLDIR)/share/uzbl/examples/config/config > $(INSTALLDIR)/share/uzbl/examples/config/config
+	rm $(INSTALLDIR)/share/uzbl/examples/config/config.bak
 	install -D -m755 uzbl-core $(INSTALLDIR)/bin/uzbl-core
 
 install-uzbl-browser:
 	install -D -m755 src/uzbl-browser $(INSTALLDIR)/bin/uzbl-browser
 	install -D -m755 examples/data/scripts/uzbl-cookie-daemon $(INSTALLDIR)/bin/uzbl-cookie-daemon
 	install -D -m755 examples/data/scripts/uzbl-event-manager $(INSTALLDIR)/bin/uzbl-event-manager
-	sed -i 's#^PREFIX=.*#PREFIX=$(RUN_PREFIX)#' $(INSTALLDIR)/bin/uzbl-browser
-	sed -i "s#^PREFIX = .*#PREFIX = '$(RUN_PREFIX)'#" $(INSTALLDIR)/bin/uzbl-event-manager
+	mv $(INSTALLDIR)/bin/uzbl-browser{,.bak}
+	sed 's#^PREFIX=.*#PREFIX=$(RUN_PREFIX)#' < $(INSTALLDIR)/bin/uzbl-browser > $(INSTALLDIR)/bin/uzbl-browser.bak
+	rm $(INSTALLDIR)/bin/uzbl-browser.bak
+	mv $(INSTALLDIR)/bin/uzbl-event-manager{,.bak}
+	sed "s#^PREFIX = .*#PREFIX = '$(RUN_PREFIX)'#" < $(INSTALLDIR)/bin/uzbl-event-manager > $(INSTALLDIR)/bin/uzbl-event-manager.bak
+	rm $(INSTALLDIR)/bin/uzbl-event-manager.bak
 
 install-uzbl-tabbed:
 	install -D -m755 examples/data/scripts/uzbl-tabbed $(INSTALLDIR)/bin/uzbl-tabbed

commit 22a5c6787a13c5cc02637f6007cce3cd9aa7522f

fixed regression introduced by 497a01b283266047d3496db14571be600a8d732d
i.e. fixed 'sed -i gnuism' fix

Makefile
@@ -99,7 +99,7 @@ install-uzbl-core: all install-dirs
 	cp -r examples $(INSTALLDIR)/share/uzbl/
 	chmod 755 $(INSTALLDIR)/share/uzbl/examples/data/scripts/*
 	mv $(INSTALLDIR)/share/uzbl/examples/config/config{,.bak}
-	sed 's#^set prefix.*=.*#set prefix     = $(RUN_PREFIX)#' < $(INSTALLDIR)/share/uzbl/examples/config/config > $(INSTALLDIR)/share/uzbl/examples/config/config
+	sed 's#^set prefix.*=.*#set prefix     = $(RUN_PREFIX)#' < $(INSTALLDIR)/share/uzbl/examples/config/config.bak > $(INSTALLDIR)/share/uzbl/examples/config/config
 	rm $(INSTALLDIR)/share/uzbl/examples/config/config.bak
 	install -m755 uzbl-core $(INSTALLDIR)/bin/uzbl-core
 
@@ -108,10 +108,10 @@ install-uzbl-browser: install-dirs
 	install -m755 examples/data/scripts/uzbl-cookie-daemon $(INSTALLDIR)/bin/uzbl-cookie-daemon
 	install -m755 examples/data/scripts/uzbl-event-manager $(INSTALLDIR)/bin/uzbl-event-manager
 	mv $(INSTALLDIR)/bin/uzbl-browser{,.bak}
-	sed 's#^PREFIX=.*#PREFIX=$(RUN_PREFIX)#' < $(INSTALLDIR)/bin/uzbl-browser > $(INSTALLDIR)/bin/uzbl-browser.bak
+	sed 's#^PREFIX=.*#PREFIX=$(RUN_PREFIX)#' < $(INSTALLDIR)/bin/uzbl-browser.bak > $(INSTALLDIR)/bin/uzbl-browser
 	rm $(INSTALLDIR)/bin/uzbl-browser.bak
 	mv $(INSTALLDIR)/bin/uzbl-event-manager{,.bak}
-	sed "s#^PREFIX = .*#PREFIX = '$(RUN_PREFIX)'#" < $(INSTALLDIR)/bin/uzbl-event-manager > $(INSTALLDIR)/bin/uzbl-event-manager.bak
+	sed "s#^PREFIX = .*#PREFIX = '$(RUN_PREFIX)'#" < $(INSTALLDIR)/bin/uzbl-event-manager.bak > $(INSTALLDIR)/bin/uzbl-event-manager
 	rm $(INSTALLDIR)/bin/uzbl-event-manager.bak
 
 install-uzbl-tabbed: install-dirs