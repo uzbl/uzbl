cfd3ff8fcff8aad7b57d8b757c2401f58aebd561
Ben Boeckel <MathStuf@gmail.com>
2010-08-21 12:02:43 -0400

    Use a case statement in the wmii script

--- examples/data/scripts/instance-select-wmii.sh
+++ examples/data/scripts/instance-select-wmii.sh
@@ -13,29 +13,34 @@
 
 source $UZBL_UTIL_DIR/dmenu.sh
 
-if [ "$1" == 'list' ]; then
-	list=
-	# get window id's of uzbl clients. we could also get the label in one shot but it's pretty tricky
-	for i in $(wmiir read /tag/sel/index | grep uzbl |cut -d ' ' -f2); do
-		label=$(wmiir read /client/$i/label)
-		list="$list$i : $label\n"
-	done
-	window=$(echo -e "$list" | $DMENU | cut -d ' ' -f1)
-	wmiir xwrite /tag/sel/ctl "select client $window"
-elif [ "$1" == 'next' ]; then
-	current=$(wmiir read /client/sel/ctl | head -n 1)
-	# find the next uzbl window and focus it
-	next=$(wmiir read /tag/sel/index | grep -A 10000 " $current " | grep -m 1 uzbl | cut -d ' ' -f2)
-	if [ -n "$next" ]; then
-		wmiir xwrite /tag/sel/ctl "select client $next"
-	fi
-elif [ "$1" == 'prev' ]; then
-	current=$(wmiir read /client/sel/ctl | head -n 1)
-	prev=$(wmiir read /tag/sel/index | grep -B 10000 " $current " | tac | grep -m 1 uzbl | cut -d ' ' -f2)
-	if [ -n "$prev" ]; then
-		wmiir xwrite /tag/sel/ctl "select client $prev"
-	fi
-else
-	echo "\$1 not valid" >&2
-	exit 2
-fi
+case "$1" in
+    "list" )
+        list=
+        # get window id's of uzbl clients. we could also get the label in one shot but it's pretty tricky
+        for i in $(wmiir read /tag/sel/index | grep uzbl |cut -d ' ' -f2); do
+            label=$(wmiir read /client/$i/label)
+            list="$list$i : $label\n"
+        done
+        window=$(echo -e "$list" | $DMENU | cut -d ' ' -f1)
+        wmiir xwrite /tag/sel/ctl "select client $window"
+        ;;
+    "next" )
+        current=$(wmiir read /client/sel/ctl | head -n 1)
+        # find the next uzbl window and focus it
+        next=$(wmiir read /tag/sel/index | grep -A 10000 " $current " | grep -m 1 uzbl | cut -d ' ' -f2)
+        if [ -n "$next" ]; then
+            wmiir xwrite /tag/sel/ctl "select client $next"
+        fi
+        ;;
+    "prev" )
+        current=$(wmiir read /client/sel/ctl | head -n 1)
+        prev=$(wmiir read /tag/sel/index | grep -B 10000 " $current " | tac | grep -m 1 uzbl | cut -d ' ' -f2)
+        if [ -n "$prev" ]; then
+            wmiir xwrite /tag/sel/ctl "select client $prev"
+        fi
+        ;;
+    * )
+        echo "$1 not valid" >&2
+        exit 2
+        ;;
+esac
