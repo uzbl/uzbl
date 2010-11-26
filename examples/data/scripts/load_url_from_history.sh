b099e55133502e033f762ccd753a02404eaecf58
Jochen Sprickerhof <jochen@sprickerhof.de>
2010-11-26 18:07:54 +0100

    fix bashism

--- examples/data/scripts/load_url_from_history.sh
+++ examples/data/scripts/load_url_from_history.sh
@@ -13,7 +13,7 @@ DMENU_OPTIONS="xmms vertical resize"
 # goto=$(awk '{print $3}' $history_file | sort -u | dmenu -i)
 if [ -z "$DMENU_HAS_VERTICAL" ]; then
     current=$(tail -n 1 "$UZBL_HISTORY_FILE" | awk '{print $3}');
-    goto=$((echo $current; awk '{print $3}' "$UZBL_HISTORY_FILE" | grep -v "^$current\$" | sort -u) | $DMENU)
+    goto=$( (echo $current; awk '{print $3}' "$UZBL_HISTORY_FILE" | grep -v "^$current\$" | sort -u) | $DMENU)
 else
     # choose an item in reverse order, showing also the date and page titles
     # pick the last field from the first 3 fields. this way you can pick a url (prefixed with date & time) or type just a new url.
