9e295f95eea8c80d6c5bf6e8b85a42630a72d44f
Brendan Taylor <whateley@gmail.com>
2010-12-03 22:07:36 -0700

    per-site-settings.py obsoletes the userscript examples

--- examples/data/scripts/userscripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-scripts_dir="$XDG_DATA_HOME/uzbl/userscripts"
-
-for SCRIPT in $(grep -rlx "\s*//\s*==UserScript==\s*" "$scripts_dir")
-do
-	$XDG_DATA_HOME/uzbl/scripts/userscript.sh "$UZBL_FIFO" "$UZBL_URI" "$SCRIPT"
-done
