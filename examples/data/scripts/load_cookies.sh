a24bd9a761773aad6a9512da5c3524f07c32d689
keis <keijser@gmail.com>
2010-12-17 23:42:02 +0100

    use sync_spawn_exec to load cookies

--- examples/data/scripts/load_cookies.sh
+++ examples/data/scripts/load_cookies.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-if [ "$1" != "" ]; then
-	cookie_file=$1
+if [ "$8" != "" ]; then
+	cookie_file=$8
 else
 	cookie_file=${XDG_DATA_HOME:-$HOME/.local/share}/uzbl/cookies.txt
 fi
-
-echo $cookie_file
 
 awk -F \\t '
 BEGIN {
@@ -19,4 +17,4 @@ printf("add_cookie \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\"\n", substr($1,lengt
 $0 !~ /^#/ {
 printf("add_cookie \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\"\n", $1, $3, $6, $7, scheme[$4], $5)
 }
-' $cookie_file > $UZBL_FIFO
+' $cookie_file
