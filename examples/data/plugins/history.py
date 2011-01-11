08b45348831fadadeaac4cefb03eb11538c7aa0c
keis <keijser@gmail.com>
2011-01-11 12:51:00 +0100

    fix bug in history search
    disable keycmd set in search as this will not work from stacked bindings

--- examples/data/plugins/history.py
+++ examples/data/plugins/history.py
@@ -57,7 +57,7 @@ class History(object):
 
     def search(self, key):
         self.search_key = key
-        return self.prev()
+        self.cursor = None
 
     def add(self, cmd):
         if self._temporary:
@@ -106,7 +106,7 @@ def history_next(uzbl, _x):
     uzbl.logger.debug('NEXT %s' % uzbl.history)
 
 def history_search(uzbl, key):
-    uzbl.set_keycmd(history.search(key))
+    uzbl.history.search(key)
     uzbl.logger.debug('SEARCH %s %s' % (key, uzbl.history))
 
 end_messages = ('Look behind you, A three-headed monkey!', 'error #4: static from nylon underwear.', 'error #5: static from plastic slide rules.', 'error #6: global warming.', 'error #9: doppler effect.', 'error #16: somebody was calculating pi on the server.', 'error #19: floating point processor overflow.', 'error #21: POSIX compliance problem.', 'error #25: Decreasing electron flux.', 'error #26: first Saturday after first full moon in Winter.', 'error #64: CPU needs recalibration.', 'error #116: the real ttys became pseudo ttys and vice-versa.', 'error #229: wrong polarity of neutron flow.', 'error #330: quantum decoherence.', 'error #388: Bad user karma.', 'error #407: Route flapping at the NAP.', 'error #435: Internet shut down due to maintenance.')
