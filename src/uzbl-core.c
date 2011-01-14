3c1697ff57a8fb4b01969da16d66ed37f0652d9b
Brendan Taylor <whateley@gmail.com>
2011-01-13 17:44:40 -0700

    don't make the window bigger to accomodate a long status_format

--- src/uzbl-core.c
+++ src/uzbl-core.c
@@ -1943,6 +1943,12 @@ create_window () {
     gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
     gtk_widget_set_name (window, "Uzbl browser");
 
+    /* if the window has been made small, it shouldn't try to resize itself due
+     * to a long statusbar. */
+    GdkGeometry hints;
+    hints.min_width = 1;
+    gtk_window_set_geometry_hints (GTK_WINDOW (window), window, &hints, GDK_HINT_MIN_SIZE);
+
     g_signal_connect (G_OBJECT (window), "destroy",         G_CALLBACK (destroy_cb),         NULL);
     g_signal_connect (G_OBJECT (window), "configure-event", G_CALLBACK (configure_event_cb), NULL);
 
