751bcfab6c3b13475873fdaac9f2d6ef2e48fb65
Ben Boeckel <MathStuf@gmail.com>
2011-03-09 23:26:12 -0500

    Check the bookmark file for duplicates
    Check to see if the url is already bookmarked. If it is, grab its tags,
    merge them with the new ones, sort them, and then put it back in the
    bookmark file.

--- examples/data/scripts/insert_bookmark.sh
+++ examples/data/scripts/insert_bookmark.sh
@@ -11,5 +11,27 @@ tags="$( zenity --entry --text="Enter space-separated tags for bookmark $UZBL_UR
 exitstatus="$?"
 [ "$exitstatus" -eq 0 ] || exit "$exitstatus"
 
-# TODO: check if already exists, if so, and tags are different: ask if you want to replace tags
-print "$UZBL_URI $tags\n" >> "$UZBL_BOOKMARKS_FILE"
+# Get the fields from the entry
+url=$( echo -n "$entry" | cut -d "	" -f 1 )
+title=$( echo -n "$entry" | cut -d "	" -f 2 )
+tags=$( echo -n "$entry" | cut -d "	" -f 3 )
+
+# See if the url is in the file already
+cur=$( sed -n -e "\<^$eurl<p" "$UZBL_BOOKMARKS_FILE" | head -n 1 )
+if [ -n "$cur" ]; then
+    burl=$( echo -n "$cur" | cut -d "	" -f 1 )
+    btitle=$( echo -n "$cur" | cut -d "	" -f 2 )
+    btags=$( echo -n "$cur" | cut -d "	" -f 3 )
+
+    # Remove the current entry from the bookmarks file
+    sed -i -e "\<^$url\t<d" "$UZBL_BOOKMARKS_FILE"
+
+    # Append the exiting tags
+    tags="$tags $btags"
+fi
+
+# Remove duplicate tags (also sorts)
+tags=$( echo -n "$tags" | tr ' ' '\n' | sort -u | tr '\n' ' ' )
+
+# Put the tags in the file
+print "$url	$title	$tags\n" >> "$UZBL_BOOKMARKS_FILE"
