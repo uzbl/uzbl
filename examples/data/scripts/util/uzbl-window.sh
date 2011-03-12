07ef26f830a49833e49d0bb53f22a6ad25fa3c46
Ben Boeckel <MathStuf@gmail.com>
2011-03-11 20:13:56 -0500

    Fix window geometry sed expression

--- examples/data/scripts/util/uzbl-window.sh
+++ examples/data/scripts/util/uzbl-window.sh
@@ -2,9 +2,9 @@
 # uzbl window detection
 
 UZBL_WIN_POS="$( xwininfo -id "$UZBL_XID" | \
-                 sed -n -e 's/Corners:[ ]*[+-]\([0-9]*\)[+-]\([0-9]*\).*$/\1 \2/p' )"
+                 sed -n -e '[ ]*s/Corners:[ ]*[+-]\([0-9]*\)[+-]\([0-9]*\).*$/\1 \2/p' )"
 UZBL_WIN_SIZE="$( xwininfo -id "$UZBL_XID" | \
-                  sed -n -e 's/-geometry[ ]*\([0-9]*\)x\([0-9]*\).*$/\1 \2/p' )"
+                  sed -n -e '[ ]*s/-geometry[ ]*\([0-9]*\)x\([0-9]*\).*$/\1 \2/p' )"
 UZBL_WIN_POS_X="$( printf "$UZBL_WIN_POS" | cut -d ' ' -f 1 )"
 UZBL_WIN_POS_Y="$( printf "$UZBL_WIN_POS" | cut -d ' ' -f 2 )"
 UZBL_WIN_WIDTH="$( printf "$UZBL_WIN_SIZE" | cut -d ' ' -f 1 )"
