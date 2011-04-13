cd737560c394196a2fff99afe5acc8d5c4ccb93a
Brendan Taylor <whateley@gmail.com>
2011-04-12 21:16:07 -0600

    use 'echo' instead of 'print' in examples/data/scripts/*temp*.sh

--- examples/data/scripts/insert_temp.sh
+++ examples/data/scripts/insert_temp.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 . "$UZBL_UTIL_DIR/uzbl-dir.sh"
-. "$UZBL_UTIL_DIR/uzbl-util.sh"
 
 >> "$UZBL_TEMPS_FILE" || exit 1
 
-print "$UZBL_URI $UZBL_TITLE\n" >> "$UZBL_TEMPS_FILE"
+echo "$UZBL_URI $UZBL_TITLE" >> "$UZBL_TEMPS_FILE"
