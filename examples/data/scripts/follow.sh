37d3e9056791c97ea057d87bcb256dde9f69f1c5
keis <keijser@gmail.com>
2011-04-29 20:35:49 +0200

    use focus_element instead of form active

--- examples/data/scripts/follow.sh
+++ examples/data/scripts/follow.sh
@@ -4,7 +4,7 @@
 case "$1" in
     XXXEMIT_FORM_ACTIVEXXX)
         # a form element was selected
-        printf 'event FORM_ACTIVE\nevent KEYCMD_CLEAR\n' > "$UZBL_FIFO"
+        printf 'event KEYCMD_CLEAR\n' > "$UZBL_FIFO"
         ;;
     XXXRESET_MODEXXX)
         # a link was selected, reset uzbl's input mode
