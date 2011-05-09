44b97f68f3e14f65a21d9c13d6ee50a2112be9c8
Brendan Taylor <whateley@gmail.com>
2011-05-09 08:24:04 -0600

    dear uzbl, please listen to the user when they tell you what size to be.

--- src/uzbl-core.c
+++ src/uzbl-core.c
@@ -1529,11 +1529,10 @@ create_window() {
     gtk_window_set_has_resize_grip (GTK_WINDOW (window), FALSE);
 #endif
 
-    /* if the window has been made small, it shouldn't try to resize itself due
-     * to a long statusbar. */
+    /* the window should never make itself bigger. */
     GdkGeometry hints;
-    hints.min_height = -1;
-    hints.min_width  =  1;
+    hints.min_height = 1;
+    hints.min_width  = 1;
     gtk_window_set_geometry_hints (GTK_WINDOW (window), window, &hints, GDK_HINT_MIN_SIZE);
 
     g_signal_connect (G_OBJECT (window), "destroy",         G_CALLBACK (destroy_cb),         NULL);
