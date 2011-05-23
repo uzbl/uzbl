98ba6d72af3c078cbcffc89902969e4ece5a2493
Brendan Taylor <whateley@gmail.com>
2011-05-23 05:17:19 +0000

    don't repeat yourself.

--- src/callbacks.c
+++ src/callbacks.c
@@ -874,43 +874,32 @@ download_cb(WebKitWebView *web_view, WebKitDownload *download, gpointer user_dat
     return TRUE;
 }
 
-gboolean
-scroll_vert_cb(GtkAdjustment *adjust, void *w)
-{
-    (void) w;
-
+void
+send_scroll_event(int type, GtkAdjustment *adjust) {
     gdouble value = gtk_adjustment_get_value(adjust);
     gdouble min = gtk_adjustment_get_lower(adjust);
     gdouble max = gtk_adjustment_get_upper(adjust);
     gdouble page = gtk_adjustment_get_page_size(adjust);
 
-    send_event (SCROLL_VERT, NULL,
+    send_event (type, NULL,
         TYPE_FLOAT, value,
         TYPE_FLOAT, min,
         TYPE_FLOAT, max,
         TYPE_FLOAT, page,
         NULL);
+}
 
+gboolean
+scroll_vert_cb(GtkAdjustment *adjust, void *w) {
+    (void) w;
+    send_scroll_event(SCROLL_VERT, adjust);
     return (FALSE);
 }
 
 gboolean
-scroll_horiz_cb(GtkAdjustment *adjust, void *w)
-{
+scroll_horiz_cb(GtkAdjustment *adjust, void *w) {
     (void) w;
-
-    gdouble value = gtk_adjustment_get_value(adjust);
-    gdouble min = gtk_adjustment_get_lower(adjust);
-    gdouble max = gtk_adjustment_get_upper(adjust);
-    gdouble page = gtk_adjustment_get_page_size(adjust);
-
-    send_event (SCROLL_HORIZ, NULL,
-        TYPE_FLOAT, value,
-        TYPE_FLOAT, min,
-        TYPE_FLOAT, max,
-        TYPE_FLOAT, page,
-        NULL);
-
+    send_scroll_event(SCROLL_HORIZ, adjust);
     return (FALSE);
 }
 
