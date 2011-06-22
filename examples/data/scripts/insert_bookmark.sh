025cabbaf7dcc62df1217e1eaf96055bbdb733cc
Dennis Hodapp <thehodapp@gmail.com>
2011-06-21 19:31:06 -0500

    insert_bookmark.sh checks for already existing bookmarks

--- examples/data/scripts/insert_bookmark.sh
+++ examples/data/scripts/insert_bookmark.sh
@@ -6,9 +6,20 @@
 
 which zenity >/dev/null 2>&1 || exit 2
 
-tags="$( zenity --entry --text="Enter space-separated tags for bookmark $UZBL_URI:" )"
-exitstatus="$?"
-[ "$exitstatus" -eq 0 ] || exit "$exitstatus"
+# Escape all special characters in sed
+uri_sanitized="$( echo "$UZBL_URI" | sed 's/\(\/\|\\\|&\)/\\&/g' )"
+oldtags="$( grep "^$UZBL_URI" "$UZBL_BOOKMARKS_FILE" | sed -n 's/^'$uri_sanitized' //p' )"
 
-# TODO: check if already exists, if so, and tags are different: ask if you want to replace tags
+if [ "${PIPESTATUS[0]}" == 0 ]; then
+    zenity --title "Replace Bookmark" --question --text="A bookmark for this website already exists with tags: $oldtags. \
+\nWould you like to replace existing bookmark with this one\?"
+    [ "$?" -eq 0 ] || exit 1
+    grep -v "^$UZBL_URI" "$UZBL_BOOKMARKS_FILE" > bookmarks.tmp
+    mv bookmarks.tmp "$UZBL_BOOKMARKS_FILE" 
+    rm bookmarks.tmp
+fi
+
+tags="$( zenity --title "Add Bookmark" --entry --text="Enter space-separated tags for bookmark $UZBL_URI:" )"
+[ "$?" -eq 0 ] || exit 1
+
 echo "$UZBL_URI $tags" >> "$UZBL_BOOKMARKS_FILE"
