8c3950297d7f693ec4f3ca0b0ea8053b1dd0722d
David Keijser <keijser@gmail.com>
2011-08-11 07:00:39 +0200

    port mode to a newstyle plugin

--- uzbl/plugins/mode.py
+++ uzbl/plugins/mode.py
@@ -4,69 +4,60 @@ import uzbl.plugins.config
 from .on_set import OnSetPlugin
 from .config import Config
 from uzbl.arguments import splitquoted
-
-def parse_mode_config(uzbl, args):
-    '''Parse `MODE_CONFIG <mode> <var> = <value>` event and update config if
-    the `<mode>` is the current mode.'''
-
-    args = splitquoted(args)
-    assert len(args) >= 3, 'missing mode config args'
-    mode = args[0]
-    key = args[1]
-    assert args[2] == '=', 'invalid mode config set syntax'
-    value = args.raw(3).strip()
-
-    uzbl.mode_config[mode][key] = value
-    config = Config[uzbl]
-    if config.get('mode', None) == mode:
-        config[key] = value
+from uzbl.ext import PerInstancePlugin
 
 
-def default_mode_updated(uzbl, var, mode):
-    config = Config[uzbl]
-    if mode and not config.get('mode', None):
-        logger.debug('setting mode to default %r' % mode)
-        config['mode'] = mode
+class ModePlugin(PerInstancePlugin):
+    def __init__(self, uzbl):
+        super(ModePlugin, self).__init__(uzbl)
+        self.mode_config = defaultdict(dict)
+        uzbl.connect('MODE_CONFIG', self.parse_mode_config)
+        uzbl.connect('MODE_CONFIRM', self.confirm_change)
+        OnSetPlugin[uzbl].on_set('mode', self.mode_updated, False)
+        OnSetPlugin[uzbl].on_set('default_mode', self.default_mode_updated, False)
 
+    def cleanup(self):
+        self.mode_config.clear()
 
-def mode_updated(uzbl, var, mode):
-    config = Config[uzbl]
-    if not mode:
-        mode = config.get('default_mode', 'command')
-        logger.debug('setting mode to default %r' % mode)
-        config['mode'] = mode
-        return
+    def parse_mode_config(self, args):
+        '''Parse `MODE_CONFIG <mode> <var> = <value>` event and update config
+        if the `<mode>` is the current mode.'''
 
-    # Load mode config
-    mode_config = uzbl.mode_config.get(mode, None)
-    if mode_config:
-        config.update(mode_config)
+        args = splitquoted(args)
+        assert len(args) >= 3, 'missing mode config args'
+        mode = args[0]
+        key = args[1]
+        assert args[2] == '=', 'invalid mode config set syntax'
+        value = args.raw(3).strip()
 
-    uzbl.send('event MODE_CONFIRM %s' % mode)
+        self.mode_config[mode][key] = value
+        config = Config[self.uzbl]
+        if config.get('mode', None) == mode:
+            config[key] = value
 
+    def default_mode_updated(self, var, mode):
+        config = Config[self.uzbl]
+        if mode and not config.get('mode', None):
+            logger.debug('setting mode to default %r' % mode)
+            config['mode'] = mode
 
-def confirm_change(uzbl, mode):
-    config = Config[uzbl]
-    if mode and config.get('mode', None) == mode:
-        uzbl.event('MODE_CHANGED', mode)
+    def mode_updated(self, var, mode):
+        config = Config[self.uzbl]
+        if not mode:
+            mode = config.get('default_mode', 'command')
+            logger.debug('setting mode to default %r' % mode)
+            config['mode'] = mode
+            return
 
+        # Load mode config
+        mode_config = self.mode_config.get(mode, None)
+        if mode_config:
+            config.update(mode_config)
 
-# plugin init hook
-def init(uzbl):
+        self.uzbl.send('event MODE_CONFIRM %s' % mode)
 
-    # Usage `uzbl.mode_config[mode][key] = value`
-    export(uzbl, 'mode_config', defaultdict(dict))
+    def confirm_change(self, mode):
+        config = Config[self.uzbl]
+        if mode and config.get('mode', None) == mode:
+            self.uzbl.event('MODE_CHANGED', mode)
 
-    connect_dict(uzbl, {
-        'MODE_CONFIG':  parse_mode_config,
-        'MODE_CONFIRM': confirm_change,
-    })
-
-# plugin after hook
-def after(uzbl):
-    OnSetPlugin[uzbl].on_set('mode', mode_updated)
-    OnSetPlugin[uzbl].on_set('default_mode', default_mode_updated)
-
-# plugin cleanup hook
-def cleanup(uzbl):
-    uzbl.mode_config.clear()
