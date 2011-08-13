fc4f610f9d12f39f40a83f161712e05cc6eabc72
keis <keijser@gmail.com>
2011-08-14 00:12:30 +0200

    refactor progress_bar to use new plugin system

--- uzbl/plugins/progress_bar.py
+++ uzbl/plugins/progress_bar.py
@@ -1,96 +1,94 @@
 from .config import Config
 
-UPDATES = 0
+from uzbl.ext import PerInstancePlugin
 
-def update_progress(uzbl, progress=None):
-    '''Updates the progress.output variable on LOAD_PROGRESS update.
+class ProgressBar(PerInstancePlugin):
+    def __init__(self, uzbl):
+        super(ProgressBar, self).__init__(uzbl)
+        uzbl.connect('LOAD_COMMIT', lambda uri: self.update_progess())
+        uzbl.connect('LOAD_PROGRESS', self.update_progress)
+        self.updates = 0
 
-    The current substitution options are:
-        %d = done char * done
-        %p = pending char * remaining
-        %c = percent done
-        %i = int done
-        %s = -\|/ spinner
-        %t = percent pending
-        %o = int pending
-        %r = sprites
+    def update_progress(self, progress=None):
+        '''Updates the progress.output variable on LOAD_PROGRESS update.
 
-    Default configuration options:
-        progress.format  = [%d>%p]%c
-        progress.width   = 8
-        progress.done    = =
-        progress.pending =
-        progress.spinner = -\|/
-        progress.sprites = loading
-    '''
+        The current substitution options are:
+            %d = done char * done
+            %p = pending char * remaining
+            %c = percent done
+            %i = int done
+            %s = -\|/ spinner
+            %t = percent pending
+            %o = int pending
+            %r = sprites
 
-    global UPDATES
+        Default configuration options:
+            progress.format  = [%d>%p]%c
+            progress.width   = 8
+            progress.done    = =
+            progress.pending =
+            progress.spinner = -\|/
+            progress.sprites = loading
+        '''
 
-    if progress is None:
-        UPDATES = 0
-        progress = 100
+        if progress is None:
+            self.updates = 0
+            progress = 100
 
-    else:
-        UPDATES += 1
-        progress = int(progress)
+        else:
+            self.updates += 1
+            progress = int(progress)
 
-    # Get progress config vars.
-    config = Config[uzbl]
-    format = config.get('progress.format', '[%d>%p]%c')
-    width = int(config.get('progress.width', 8))
-    done_symbol = config.get('progress.done', '=')
-    pend = config.get('progress.pending', None)
-    pending_symbol = pend if pend else ' '
+        # Get progress config vars.
+        config = Config[self.uzbl]
+        format = config.get('progress.format', '[%d>%p]%c')
+        width = int(config.get('progress.width', 8))
+        done_symbol = config.get('progress.done', '=')
+        pend = config.get('progress.pending', None)
+        pending_symbol = pend if pend else ' '
 
-    # Inflate the done and pending bars to stop the progress bar
-    # jumping around.
-    if '%c' in format or '%i' in format:
-        count = format.count('%c') + format.count('%i')
-        width += (3-len(str(progress))) * count
+        # Inflate the done and pending bars to stop the progress bar
+        # jumping around.
+        if '%c' in format or '%i' in format:
+            count = format.count('%c') + format.count('%i')
+            width += (3-len(str(progress))) * count
 
-    if '%t' in format or '%o' in format:
-        count = format.count('%t') + format.count('%o')
-        width += (3-len(str(100-progress))) * count
+        if '%t' in format or '%o' in format:
+            count = format.count('%t') + format.count('%o')
+            width += (3-len(str(100-progress))) * count
 
-    done = int(((progress/100.0)*width)+0.5)
-    pending = width - done
+        done = int(((progress/100.0)*width)+0.5)
+        pending = width - done
 
-    if '%d' in format:
-        format = format.replace('%d', done_symbol * done)
+        if '%d' in format:
+            format = format.replace('%d', done_symbol * done)
 
-    if '%p' in format:
-        format = format.replace('%p', pending_symbol * pending)
+        if '%p' in format:
+            format = format.replace('%p', pending_symbol * pending)
 
-    if '%c' in format:
-        format = format.replace('%c', '%d%%' % progress)
+        if '%c' in format:
+            format = format.replace('%c', '%d%%' % progress)
 
-    if '%i' in format:
-        format = format.replace('%i', '%d' % progress)
+        if '%i' in format:
+            format = format.replace('%i', '%d' % progress)
 
-    if '%t' in format:
-        format = format.replace('%t', '%d%%' % (100-progress))
+        if '%t' in format:
+            format = format.replace('%t', '%d%%' % (100-progress))
 
-    if '%o' in format:
-        format = format.replace('%o', '%d' % (100-progress))
+        if '%o' in format:
+            format = format.replace('%o', '%d' % (100-progress))
 
-    if '%s' in format:
-        spinner = config.get('progress.spinner', '-\\|/')
-        index = 0 if progress == 100 else UPDATES % len(spinner)
-        spin = '\\\\' if spinner[index] == '\\' else spinner[index]
-        format = format.replace('%s', spin)
+        if '%s' in format:
+            spinner = config.get('progress.spinner', '-\\|/')
+            index = 0 if progress == 100 else self.updates % len(spinner)
+            spin = '\\\\' if spinner[index] == '\\' else spinner[index]
+            format = format.replace('%s', spin)
 
-    if '%r' in format:
-        sprites = config.get('progress.sprites', 'loading')
-        index = int(((progress/100.0)*len(sprites))+0.5)-1
-        sprite = '\\\\' if sprites[index] == '\\' else sprites[index]
-        format = format.replace('%r', sprite)
+        if '%r' in format:
+            sprites = config.get('progress.sprites', 'loading')
+            index = int(((progress/100.0)*len(sprites))+0.5)-1
+            sprite = '\\\\' if sprites[index] == '\\' else sprites[index]
+            format = format.replace('%r', sprite)
 
-    if config.get('progress.output', None) != format:
-        config['progress.output'] = format
-
-# plugin init hook
-def init(uzbl):
-    connect_dict(uzbl, {
-        'LOAD_COMMIT':      lambda uzbl, uri: update_progress(uzbl),
-        'LOAD_PROGRESS':    update_progress,
-    })
+        if config.get('progress.output', None) != format:
+            config['progress.output'] = format
