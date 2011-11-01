56336d06e20114f81290a03dbc1a83f74462ea60
root <root@arch-dennis.localdomain>
2011-11-01 14:55:09 -0500

    removed unecessary double quotes

--- examples/data/scripts/insert_bookmark.sh
+++ examples/data/scripts/insert_bookmark.sh
@@ -10,8 +10,8 @@ which zenity >/dev/null 2>&1 || exit 2
 if grep "^$UZBL_URI" "$UZBL_BOOKMARKS_FILE" 2>&1 > /dev/null ; then
 
     # Escape special characters (used in sed) and filter tags
-    uri_sanitized="$( echo "$UZBL_URI" | sed 's/\(\.\|\/\|\*\|\[\|\]\|\\\)/\\&/g' )"
-    oldtags="$( sed -n 's/^'$uri_sanitized' //p' < "$UZBL_BOOKMARKS_FILE" )"
+    uri_sanitized=$( echo "$UZBL_URI" | sed 's/\(\.\|\/\|\*\|\[\|\]\|\\\)/\\&/g' )
+    oldtags=$( sed -n 's/^'$uri_sanitized' //p' < "$UZBL_BOOKMARKS_FILE" )
 
     zenity --question --title="Replace Bookmark" --text="A bookmark for <b>$UZBL_URI</b> already exists with tags: \
 <b>$oldtags</b>.\n\nWould you like to replace the existing bookmark with a new one\?"
@@ -19,10 +19,10 @@ if grep "^$UZBL_URI" "$UZBL_BOOKMARKS_FILE" 2>&1 > /dev/null ; then
     [ "$?" -eq 0 ] || exit 0
 
     # Delete old bookmark
-    newfile="$( grep -v "^$UZBL_URI" "$UZBL_BOOKMARKS_FILE" )"
+    newfile=$( grep -v "^$UZBL_URI" "$UZBL_BOOKMARKS_FILE" )
     echo "$newfile" > "$UZBL_BOOKMARKS_FILE" 
 fi
 
-tags="$( zenity --entry --text="Enter space-separated tags for bookmark $UZBL_URI:" )"
+tags=$( zenity --entry --text="Enter space-separated tags for bookmark $UZBL_URI:" )
 
 echo "$UZBL_URI $tags" >> "$UZBL_BOOKMARKS_FILE"
