eff8d68812c996b2ce8cc0747935f702ba64ccf9
Brendan Taylor <whateley@gmail.com>
2011-12-13 17:58:04 -0700

    get_geometry: don't segfault if uzbl.gui.main_window is NULL

--- src/variables.c
+++ src/variables.c
@@ -535,10 +535,12 @@ get_geometry() {
     int w, h, x, y;
     GString *buf = g_string_new("");
 
-    gtk_window_get_size(GTK_WINDOW(uzbl.gui.main_window), &w, &h);
-    gtk_window_get_position(GTK_WINDOW(uzbl.gui.main_window), &x, &y);
+    if(uzbl.gui.main_window) {
+      gtk_window_get_size(GTK_WINDOW(uzbl.gui.main_window), &w, &h);
+      gtk_window_get_position(GTK_WINDOW(uzbl.gui.main_window), &x, &y);
 
-    g_string_printf(buf, "%dx%d+%d+%d", w, h, x, y);
+      g_string_printf(buf, "%dx%d+%d+%d", w, h, x, y);
+    }
 
     return g_string_free(buf, FALSE);
 }
