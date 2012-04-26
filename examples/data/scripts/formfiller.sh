328802c60d88b21c550f030d90e76b26f7f35d20
Ben Boeckel <mathstuf@gmail.com>
2012-04-25 22:23:11 -0400

    Improve the formfiller script
    Basically, the storing every profile in a single file was error prone
    and hacky. The more unix-y thing to do is just store each profile in its
    own file. This makes profiles much easier to handle.

--- examples/data/scripts/formfiller.sh
+++ examples/data/scripts/formfiller.sh
@@ -8,11 +8,9 @@
 # (empty): if file not available, new; otherwise, load
 #
 
-action=$1
-
+. "$UZBL_UTIL_DIR/editor.sh"
 . "$UZBL_UTIL_DIR/uzbl-dir.sh"
 . "$UZBL_UTIL_DIR/uzbl-util.sh"
-. "$UZBL_UTIL_DIR/editor.sh"
 
 mkdir -p "$UZBL_FORMS_DIR" || exit
 
@@ -21,140 +19,207 @@ domain="${domain%%/*}"
 
 test "$domain" || exit
 
-file="$UZBL_FORMS_DIR/$domain"
+basefile="$UZBL_FORMS_DIR/$domain"
+default_profile="default"
 
-GenForm ()
-{
-    uzbl_control 'js uzbl.formfiller.dump();\n' \
+action="$1"
+shift
+
+generate_form () {
+    uzbl_control "js uzbl.formfiller.dump();\n" \
     | awk '
         /^formfillerstart$/ {
             while (getline) {
-                if ( /^%!end/ ) exit
+                if (/^%!end/) {
+                    exit
+                }
                 print
             }
         }
     '
 }
 
-GetOption ()
-{
-    DMENU_SCHEME=formfiller
-
-    # util/dmenu.sh doesn't handle spaces in DMENU_PROMPT. a proper fix will be
-    # tricky.
-    DMENU_PROMPT="choose_profile"
+get_option () {
+    DMENU_SCHEME="formfiller"
+    DMENU_PROMPT="profile"
     DMENU_LINES=4
 
     . "$UZBL_UTIL_DIR/dmenu.sh"
 
-    if [ $(grep -c '^!profile' "$1") -gt 1 ]
-    then sed -n 's/^!profile=//p' "$1" | $DMENU
-    else sed -n 's/^!profile=//p' "$1"
-    fi
+    count=""
+
+    for x in "$basefile"*; do
+        [ "$x" = "$basefile*" ] && continue
+
+        count="1$count"
+    done
+
+    case "$count" in
+        11*)
+            DMENU_MORE_ARGS=
+            if [ -n "$DMENU_HAS_PLACEMENT" ]; then
+                . "$UZBL_UTIL_DIR/uzbl-window.sh"
+
+                DMENU_MORE_ARGS="$DMENU_PLACE_X $(( $UZBL_WIN_POS_X + 1 )) $DMENU_PLACE_Y $(( $UZBL_WIN_POS_Y + $UZBL_WIN_HEIGHT - 184 )) $DMENU_PLACE_WIDTH $UZBL_WIN_WIDTH"
+            fi
+
+            ls "$basefile"* | sed -e 's!^'"$basefile"'\.!!' | $DMENU $DMENU_MORE_ARGS
+            ;;
+        1)
+            echo "$basefile"*
+            ;;
+        *)
+            ;;
+    esac
 }
 
-ParseProfile ()
-{
-    sed "/^>/d; /^!profile=$1$/,/^!/!d; /^!/d"
-}
-
-ParseFields ()
-{
+parse_profile () {
     awk '/^%/ {
 
-        sub ( /%/, "" )
-        gsub ( /\\/, "\\\\\\\\" )
-        gsub ( /@/, "\\@" )
-        gsub ( /"/, "\\\"" )
+        sub(/%/, "")
+        gsub(/\\/, "\\\\\\\\")
+        gsub(/@/, "\\@")
+        gsub(/"/, "\\\"")
 
-        split( $0, parts, /\(|\)|\{|\}/ )
+        split($0, parts, /\(|\)|\{|\}/)
 
         field = $0
-        sub ( /[^:]*:/, "", field )
+        sub(/[^:]*:/, "", field)
 
-        if ( parts[2] ~ /^(checkbox|radio)$/ )
-            printf( "js uzbl.formfiller.insert(\"%s\",\"%s\",\"%s\",%s);\n",
-                    parts[1], parts[2], parts[3], field )
-
-        else if ( parts[2] == "textarea" ) {
+        if (parts[2] ~ /^(checkbox|radio)$/) {
+            printf("js uzbl.formfiller.insert(\"%s\",\"%s\",\"%s\",%s);\n",
+                parts[1], parts[2], parts[3], field)
+        } else if (parts[2] == "textarea") {
             field = ""
             while (getline) {
-                if ( /^%/ ) break
-                sub ( /^\\/, "" )
+                if (/^%/) {
+                    break
+                }
+                sub(/^\\/, "")
                 # JavaScript escape
-                gsub ( /\\/, "\\\\\\\\" )
-                gsub ( /"/, "\\\"" )
+                gsub(/\\/, "\\\\\\\\")
+                gsub(/"/, "\\\"")
                 # To support the possibility of the last line of the textarea
                 # not being terminated by a newline, we add the newline here.
                 # The "if (field)" is so that this does not happen in the first
                 # iteration.
-                if (field) field = field "\\n"
+                if (field) {
+                    field = field "\\n"
+                }
                 field = field $0
             }
             # Uzbl escape
-            gsub ( /\\/, "\\\\\\\\", field )
-            gsub ( /@/, "\\@", field )
-            printf( "js uzbl.formfiller.insert(\"%s\",\"%s\",\"%s\",0);\n",
-                parts[1], parts[2], field )
+            gsub(/\\/, "\\\\\\\\", field)
+            gsub(/@/, "\\@", field)
+            printf("js uzbl.formfiller.insert(\"%s\",\"%s\",\"%s\",0);\n",
+                parts[1], parts[2], field)
+        } else {
+            printf("js uzbl.formfiller.insert(\"%s\",\"%s\",\"%s\",0);\n",
+                parts[1], parts[2], field)
         }
-
-        else
-            printf( "js uzbl.formfiller.insert(\"%s\",\"%s\",\"%s\",0);\n",
-                    parts[1], parts[2], field )
-
 
     }'
 }
 
-New ()
-{
-    { echo '!profile=NAME_THIS_PROFILE'
-      GenForm | sed 's/^!/\\!/'
-      echo '!'
-    } >> "$file"
+new_profile () {
+    file="$1"
+    shift
+
+    if [ -z "$file" ]; then
+        profile="$default_profile"
+
+        while true; do
+            profile="$( Xdialog --stdout --title "New profile for $domain" --inputbox "Profile name:" 0 0 "$profile" )"
+            exitstatus="$?"
+
+            [ "$exitstatus" -eq 0 ] || exit "$exitstatus"
+
+            [ -z "$profile" ] && profile="$default_profile"
+
+            file="$basefile.$profile"
+
+            if [ -e "$file" ]; then
+                Xdialog --title "Profile already exists!" --yesno "Overwrite?" 0 0
+                exitstatus="$?"
+
+                [ "$exitstatus" -eq 0 ] && break
+            else
+                break
+            fi
+        done
+    fi
+
+    generate_form > "$file"
     chmod 600 "$file"
     $UZBL_EDITOR "$file"
 }
 
-Edit ()
-    if [ -e "$file" ]
-    then $UZBL_EDITOR "$file"
-    else New
+edit_profile () {
+    profile="$( get_option )"
+
+    [ -z "$profile" ] && profile="$default_profile"
+
+    file="$basefile.$profile"
+
+    if [ -e "$file" ]; then
+        $UZBL_EDITOR "$file"
+    else
+        new_profile "$profile"
+    fi
+}
+
+load_profile () {
+    if [ -z "$file" ]; then
+        profile="$( get_option )"
+
+        [ -z "$profile" ] && profile="$default_profile"
+
+        file="$basefile.$profile"
     fi
 
-Load ()
-{
-    test -e "$file" || exit
-
-    option=$(GetOption "$file")
-
-    case $option in *[!a-zA-Z0-9_-]*) exit 1; esac
-
-    ParseProfile $option < "$file" \
-    | ParseFields \
+    parse_profile < "$file" \
     | uzbl_send
 }
 
-Once ()
+one_time_profile ()
 {
-    tmpfile=/tmp/${0##*/}-$$-tmpfile
+    tmpfile="$XDG_SOCKET_DIR/uzbl/formfiller-${0##*/}-$$"
     trap 'rm -f "$tmpfile"' EXIT
 
-    GenForm > "$tmpfile"
+    generate_form > "$tmpfile"
     chmod 600 "$tmpfile"
 
     $UZBL_EDITOR "$tmpfile"
 
     test -e "$tmpfile" &&
-    ParseFields < "$tmpfile" \
+    parse_profile < "$tmpfile" \
     | uzbl_send
 }
 
-case $action in
-    new) New; Load ;;
-    edit) Edit; Load ;;
-    load) Load ;;
-    once) Once ;;
-    '') if [ -e "$file" ]; then Load; else New; Load; fi ;;
-    *) exit 1
+case "$action" in
+    new)
+        new_profile
+        load_profile
+        ;;
+    edit)
+        edit_profile
+        load_profile
+        ;;
+    load)
+        load_profile
+        ;;
+    once)
+        one_time_profile
+        ;;
+    '')
+        if [ -e "$file" ]; then
+            load_profile
+        else
+            new_profile
+            load_profile
+        fi
+        ;;
+    *)
+        exit 1
+        ;;
 esac
