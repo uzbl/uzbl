276a812e78b8fe82fcfb1886a3b5a8689cdec317
Ben Boeckel <mathstuf@gmail.com>
2012-04-25 21:47:31 -0400

    Use uzbl-util in follow.sh

--- examples/data/scripts/follow.sh
+++ examples/data/scripts/follow.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 # This scripts acts on the return value of followLinks in follow.js
 
+. "$UZBL_UTIL_DIR/uzbl-util.sh"
+
 result=$1
 shift
 
 case "$result" in
     XXXFORM_ACTIVEXXX)
         # a form element was selected
-        echo 'event KEYCMD_CLEAR' > "$UZBL_FIFO"
+        uzbl_control 'event KEYCMD_CLEAR\n'
         ;;
     XXXRESET_MODEXXX)
         # a link was selected, reset uzbl's input mode
-        printf 'set mode=\nevent KEYCMD_CLEAR\n' > "$UZBL_FIFO"
+        uzbl_control 'set mode=\nevent KEYCMD_CLEAR\n'
         ;;
     XXXNEW_WINDOWXXX*)
-        echo "set mode=
-event KEYCMD_CLEAR
-event NEW_WINDOW $@" > "$UZBL_FIFO"
+        uzbl_control "set mode=\nevent KEYCMD_CLEAR\nevent NEW_WINDOW $@\n"
         ;;
     XXXRETURNED_URIXXX*)
         uriaction=$1
         uri=${result#XXXRETURNED_URIXXX}
 
-        printf 'set mode=\nevent KEYCMD_CLEAR\n' > "$UZBL_FIFO"
+        uzbl_control 'set mode=\nevent KEYCMD_CLEAR\n'
 
         [ -z "$uri" ] && exit
 
         case "$uriaction" in
             set)
-                printf 'uri '"$uri"'\n' | sed -e 's/@/\\@/' > "$UZBL_FIFO"
+                print 'uri '"$uri"'\n' | sed -e 's/@/\\@/' | uzbl_send
                 ;;
             clipboard)
-                printf "$uri" | xclip
+                print "$uri" | xclip
                 ;;
         esac
 esac
