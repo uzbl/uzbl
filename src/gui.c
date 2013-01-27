62943712a60991e85202493b209193e2d6fe026b
Ben Boeckel <mathstuf@gmail.com>
2013-01-27 12:48:32 -0500

    Stylize set_window_property

--- src/gui.c
+++ src/gui.c
@@ -43,16 +43,18 @@ uzbl_gui_init (gboolean plugmode)
 }
 
 void
-set_window_property(const gchar* prop, const gchar* value) {
-    if(GTK_IS_WIDGET(uzbl.gui.main_window)) {
-        gdk_property_change(
+set_window_property (const gchar *prop, const gchar *value)
+{
+    if (GTK_IS_WIDGET (uzbl.gui.main_window))
+    {
+        gdk_property_change (
             gtk_widget_get_window (GTK_WIDGET (uzbl.gui.main_window)),
-            gdk_atom_intern_static_string(prop),
-            gdk_atom_intern_static_string("STRING"),
+            gdk_atom_intern_static_string (prop),
+            gdk_atom_intern_static_string ("STRING"),
             8,
             GDK_PROP_MODE_REPLACE,
-            (const guchar*)value,
-            strlen(value));
+            (const guchar *)value,
+            strlen (value));
     }
 }
 
