387e3a3a34d26234c9e619425a1ec68322cda77c
Ben Boeckel <mathstuf@gmail.com>
2013-03-19 16:58:06 -0400

    Clean up cookie-jar.*

--- src/cookie-jar.h
+++ src/cookie-jar.h
@@ -4,8 +4,8 @@
 #include <libsoup/soup-cookie-jar.h>
 
 #define UZBL_TYPE_COOKIE_JAR            (soup_cookie_jar_socket_get_type ())
-#define UZBL_COOKIE_JAR(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), UZBL_TYPE_COOKIE_JAR,        UzblCookieJar))
-#define UZBL_COOKIE_JAR_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), UZBL_TYPE_COOKIE_JAR,         UzblCookieJarClass))
+#define UZBL_COOKIE_JAR(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), UZBL_TYPE_COOKIE_JAR, UzblCookieJar))
+#define UZBL_COOKIE_JAR_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), UZBL_TYPE_COOKIE_JAR,  UzblCookieJarClass))
 
 typedef struct {
     SoupCookieJar parent;
@@ -17,6 +17,7 @@ typedef struct {
     SoupCookieJarClass parent_class;
 } UzblCookieJarClass;
 
-UzblCookieJar *uzbl_cookie_jar_new();
+UzblCookieJar *
+uzbl_cookie_jar_new ();
 
 #endif
