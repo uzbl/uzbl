38906d98f66f30dbeec969bc4f19aed981d88aca
Ben Boeckel <mathstuf@gmail.com>
2013-03-20 22:55:31 -0400

    Nits in config.h

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@ const struct {
 { "set show_status = 1" },
 { "set title_format_long = \\@keycmd \\@TITLE - Uzbl browser <\\@NAME> > \\@SELECTED_URI" },
 { "set title_format_short = \\@TITLE - Uzbl browser <\\@NAME>" },
-{ "set max_conns = 100" }, /* WebkitGTK default: 10 */
-{ "set max_conns_host = 6" }, /* WebkitGTK default: 2 */
+{ "set max_conns = 100" }, /* WebKitGTK default: 10 */
+{ "set max_conns_host = 6" }, /* WebKitGTK default: 2 */
 { NULL }
 };
