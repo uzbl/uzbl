bb5f43b49bec49c8bf4872b5a8a4da5faa3beb0b
Ben Boeckel <mathstuf@gmail.com>
2013-03-30 06:51:53 -0400

    Simplify NULL check

--- src/commands.c
+++ src/commands.c
@@ -1833,7 +1833,7 @@ IMPLEMENT_COMMAND (set)
     ARG_CHECK (argv, 1);
 
     gchar **split = g_strsplit (argv_idx (argv, 0), "=", 2);
-    if (split[0] != NULL) {
+    if (split[0]) {
         gchar *value = split[1] ? g_strchug (split[1]) : " ";
         uzbl_variables_set (g_strstrip (split[0]), value);
     }
