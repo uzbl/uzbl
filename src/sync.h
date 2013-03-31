56c0fa46c598581f452fa177fbaf7cb90cd4c9c3
Ben Boeckel <mathstuf@gmail.com>
2013-03-30 21:19:23 -0400

    Clean up the sync.h macros

--- src/sync.h
+++ src/sync.h
@@ -18,38 +18,26 @@ uzbl_sync_data_free (UzblSyncData *data);
 void
 uzbl_sync_close (GObject *object, GAsyncResult *result, gpointer data);
 
-#define uzbl_sync_call_void(obj, err, send, ...)                                        \
-    uzbl_sync_call_args_void (send, (obj, ## __VA_ARGS__, NULL, uzbl_sync_close, data), \
-                              send##_finish, (obj, data->result, &err))
-#define uzbl_sync_call_args_void(send, send_args,     \
-                                 finish, finish_args) \
-    do                                                \
-    {                                                 \
-        UzblSyncData *data = uzbl_sync_data_new ();   \
-                                                      \
-        send send_args;                               \
-        g_main_loop_run (data->loop);                 \
-        finish finish_args;                           \
-                                                      \
-        uzbl_sync_data_free (data);                   \
-    } while (0)
-
-#define uzbl_sync_call(res, obj, err, send, ...)                             \
-    uzbl_sync_call_args (res,                                                \
-                         send, (obj, ## __VA_ARGS__, NULL, uzbl_sync_close, data), \
-                         send##_finish, (obj, data->result, &err))
-#define uzbl_sync_call_args(result,                 \
-                            send, send_args,        \
-                            finish, finish_args)    \
+#define _uzbl_sync_call(res, obj, err, send, ...)                                        \
+    _uzbl_sync_call_args (res, send, (obj, ## __VA_ARGS__, NULL, uzbl_sync_close, data), \
+                          send##_finish, (obj, data->result, &err))
+#define _uzbl_sync_call_args(res, send, send_args,  \
+                             finish, finish_args)   \
     do                                              \
     {                                               \
         UzblSyncData *data = uzbl_sync_data_new (); \
                                                     \
         send send_args;                             \
         g_main_loop_run (data->loop);               \
-        result = finish finish_args;                \
+        res finish finish_args;                     \
                                                     \
         uzbl_sync_data_free (data);                 \
     } while (0)
+
+#define uzbl_sync_call_void(obj, err, send, ...) \
+    _uzbl_sync_call(, obj, err, send, ## __VA_ARGS__)
+
+#define uzbl_sync_call(res, obj, err, send, ...) \
+    _uzbl_sync_call(res =, obj, err, send, ## __VA_ARGS__)
 
 #endif
