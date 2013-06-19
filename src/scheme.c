ccd92a49a9421457224e2d6b6d92558d7e49f3a1
Ben Boeckel <mathstuf@gmail.com>
2013-06-19 02:05:07 -0400

    Add support for WebKit2 scheme handlers

--- src/scheme.c
+++ src/scheme.c
@@ -1,6 +1,8 @@
 #include "scheme.h"
 
-#ifndef USE_WEBKIT2
+#ifdef USE_WEBKIT2
+#include "commands.h"
+#else
 #include "scheme-request.h"
 #endif
 #include "uzbl-core.h"
@@ -15,12 +17,48 @@ uzbl_scheme_init ()
 #endif
 }
 
+#ifdef USE_WEBKIT2
+static void
+scheme_callback (WebKitURISchemeRequest *request, gpointer data);
+#endif
+
 void
 uzbl_scheme_add_handler (const gchar *scheme, const gchar *command)
 {
 #ifdef USE_WEBKIT2
-    /* TODO: Implement. */
+    WebKitWebContext *context = webkit_web_view_get_context (uzbl.gui.web_view);
+    webkit_web_context_register_uri_scheme (context, scheme, scheme_callback, g_strdup (command), g_free);
 #else
     uzbl_scheme_request_add_handler (scheme, command);
 #endif
 }
+
+#ifdef USE_WEBKIT2
+void
+scheme_callback (WebKitURISchemeRequest *request, gpointer data)
+{
+    gchar *command = (gchar *)data;
+    const gchar *uri = webkit_uri_scheme_request_get_uri (request);
+
+    GString *result = g_string_new ("");
+    GArray *args = uzbl_commands_args_new ();
+    const UzblCommand *cmd = uzbl_commands_parse (command, args);
+
+    if (cmd) {
+        uzbl_commands_args_append (args, g_strdup (uri));
+        uzbl_commands_run_parsed (cmd, args, result);
+    }
+
+    uzbl_commands_args_free (args);
+
+    gint64 len = result->len;
+    GInputStream *stream = g_memory_input_stream_new_from_data (
+        g_string_free (result, FALSE),
+        len, g_free);
+
+    webkit_uri_scheme_request_finish (request, stream, len, "text/html");
+
+    g_object_unref (stream);
+    g_free (command);
+}
+#endif
