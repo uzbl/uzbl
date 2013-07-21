9715c887f6684b828d6117e10f5960552f09aaaa
Ben Boeckel <mathstuf@gmail.com>
2013-07-20 20:58:32 -0400

    Simplify selected_url logic

--- src/gui.c
+++ src/gui.c
@@ -1151,13 +1151,10 @@ send_hover_event (const gchar *uri, const gchar *title)
 {
     g_free (uzbl.state.last_selected_url);
 
-    if (uzbl.state.selected_url) {
-        uzbl.state.last_selected_url = g_strdup (uzbl.state.selected_url);
-        g_free (uzbl.state.selected_url);
-        uzbl.state.selected_url = NULL;
-    } else {
-        uzbl.state.last_selected_url = NULL;
-    }
+    uzbl.state.last_selected_url = g_strdup (uzbl.state.selected_url);
+
+    g_free (uzbl.state.selected_url);
+    uzbl.state.selected_url = NULL;
 
     if (uzbl.state.last_selected_url && g_strcmp0 (uri, uzbl.state.last_selected_url)) {
         uzbl_events_send (LINK_UNHOVER, NULL,
