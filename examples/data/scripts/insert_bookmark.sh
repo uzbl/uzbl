ee1def5168bfde85db8e64ba2825bf271c0fa70d
Ben Boeckel <mathstuf@gmail.com>
2013-11-16 21:18:59 -0500

    Fix up insert_bookmark.sh
    Lots of bugs. Probably from an improper sync script on my part at some
    point.

--- examples/data/scripts/insert_bookmark.sh
+++ examples/data/scripts/insert_bookmark.sh
@@ -7,31 +7,35 @@
 
 which zenity >/dev/null 2>&1 || exit 2
 
-tags="$( zenity --entry --text="Enter space-separated tags for bookmark $UZBL_URI:" )"
+entry="$( zenity --text="Add bookmark. add tags after the tabulators, separated by spaces" --entry-text="$UZBL_URI	$UZBL_TITLE	" )"
 exitstatus="$?"
 [ "$exitstatus" -eq 0 ] || exit "$exitstatus"
 
 # Get the fields from the entry
-url=$( echo -n "$entry" | cut -d "	" -f 1 )
-title=$( echo -n "$entry" | cut -d "	" -f 2 )
-tags=$( echo -n "$entry" | cut -d "	" -f 3 )
+url="$( print "$entry" | cut -d "	" -f 1 )"
+title="$( print "$entry" | cut -d "	" -f 2 )"
+tags="$( print "$entry" | cut -d "	" -f 3 )"
+
+# Escape the url and title for sed
+eurl="$( print "$url" | sed -e 's!\\!\\\\!g
+                                s![][^$*./]!\\&!g
+                                s!#.*!!' )"
 
 # See if the url is in the file already
-cur=$( sed -n -e "\<^$eurl<p" "$UZBL_BOOKMARKS_FILE" | head -n 1 )
+cur="$( sed -n -e "\<^$eurl<pq" "$UZBL_BOOKMARKS_FILE" )"
 if [ -n "$cur" ]; then
-    burl=$( echo -n "$cur" | cut -d "	" -f 1 )
-    btitle=$( echo -n "$cur" | cut -d "	" -f 2 )
-    btags=$( echo -n "$cur" | cut -d "	" -f 3 )
+    burl="$( print "$cur" | cut -d "	" -f 1 )"
+    btags="$( print "$cur" | cut -d "	" -f 3 )"
 
     # Remove the current entry from the bookmarks file
-    sed -i -e "\<^$url\t<d" "$UZBL_BOOKMARKS_FILE"
+    sed_i "$UZBL_BOOKMARKS_FILE" -e "\<^$eurl\t<d"
 
     # Append the exiting tags
     tags="$tags $btags"
 fi
 
 # Remove duplicate tags (also sorts)
-tags=$( echo -n "$tags" | tr ' ' '\n' | sort -u | tr '\n' ' ' )
+tags="$( print "$tags" | tr ' ' '\n' | sort -u | tr '\n' ' ' )"
 
 # Put the tags in the file
 print "$url	$title	$tags\n" >> "$UZBL_BOOKMARKS_FILE"
