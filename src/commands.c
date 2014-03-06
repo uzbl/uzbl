fbeae61f78c0bc6773a63ba6ca39d4b1a802fcec
Ben Boeckel <mathstuf@gmail.com>
2014-03-06 03:34:51 -0500

    Set empty output if the command fails
    There's a NULL dereference later if this doesn't happen.

--- src/commands.c
+++ src/commands.c
@@ -2908,7 +2908,11 @@ run_system_command (GArray *args, char **output_stdout)
     if (output_stdout) {
         result = g_spawn_sync (NULL, (gchar **)args->data, NULL, G_SPAWN_SEARCH_PATH,
                                NULL, NULL, output_stdout, NULL, NULL, &err);
-        remove_trailing_newline (*output_stdout);
+        if (result) {
+            remove_trailing_newline (*output_stdout);
+        } else {
+            *output_stdout = g_strdup ("");
+        }
     } else {
         result = g_spawn_async (NULL, (gchar **)args->data, NULL, G_SPAWN_SEARCH_PATH,
                                 NULL, NULL, NULL, &err);
