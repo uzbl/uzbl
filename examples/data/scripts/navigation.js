e603fced00c936449f0be53d635ad8ffc4138b74
Ben Boeckel <mathstuf@gmail.com>
2014-05-02 23:27:05 -0400

    navigation: add bindings for rel="{next,prev}"

---
+++ examples/data/scripts/navigation.js
@@ -0,0 +1,48 @@
+uzbl.navigation = (function () {
+'use strict';
+
+var trySelectors = function (selectors) {
+    var elem;
+    var i;
+    for (i = 0; i < selectors.length; ++i) {
+        elem = document.querySelector(selectors[i]);
+        if (elem !== null) {
+            return elem;
+        }
+    }
+    return undefined;
+};
+
+var nextAnchor = function () {
+    return trySelectors(
+      [ 'a[rel="next"]'
+      , 'link[rel="next"]'
+      ]
+    );
+};
+
+var prevAnchor = function () {
+    return trySelectors(
+      [ 'a[rel="prev"]'
+      , 'link[rel="prev"]'
+      ]
+    );
+};
+
+var findLink = function (elem) {
+    if (typeof elem !== 'undefined') {
+        return elem.href;
+    }
+
+    return "";
+};
+
+return {
+    next: function () {
+        return findLink(nextAnchor());
+    },
+    prev: function () {
+        return findLink(prevAnchor());
+    }
+};
+}());
