c4a9eb039a731eb18263171bef01986baa4804a8
Ben Boeckel <mathstuf@gmail.com>
2015-08-27 23:33:54 -0400

    config: default forward_keys to 1
    This way a stock uzbl-core without an event manager is useful.

--- src/config.h
+++ src/config.h
@@ -13,6 +13,7 @@ default_config[] = {
 "set max_conns_host 6", /* WebKitGTK default: 2 */
 "set shell_cmd /bin/sh -c",
 "set maintain_history 1", /* Set here since the WebKit default is 1, but there's no way to get the current value. */
+"set forward_keys 1", /* Forward keys by default so that webpages work as expected without a config. */
 NULL
 };
 
