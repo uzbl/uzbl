f3fd87ea06deb2ad5a3fb857c0fb97659675e74b
David Keijser <keijser@gmail.com>
2017-02-17 18:23:30 +0100

    Add test of chain command

--- tests/core-tests.c
+++ tests/core-tests.c
@@ -55,17 +55,48 @@ test_parse_escaped_at ()
     g_assert_cmpstr (g_array_index (argv, gchar*, 0), ==, "@");
 }
 
+static void
+commands_chain_cb (GObject      *source,
+                   GAsyncResult *res,
+                   gpointer      data)
+{
+    GMainLoop *loop = (GMainLoop*) data;
+    GError *err = NULL;
+    GString *r = uzbl_commands_run_finish (source, res, &err);
+
+    g_assert_null (err);
+    g_assert_cmpstr (r->str, ==, "foobar");
+
+    g_main_loop_quit (loop);
+}
+
+static void
+test_commands_chain ()
+{
+    GMainLoop *loop = g_main_loop_new (NULL, FALSE);
+    GArray *argv = uzbl_commands_args_new ();
+    const UzblCommand *cmd = uzbl_commands_parse (
+        "chain 'print foo' 'print bar'", argv);
+
+    uzbl_commands_run_async (cmd, argv, TRUE, commands_chain_cb,
+                             (gpointer) loop);
+    g_main_loop_run (loop);
+}
+
 int
 main (int argc, char *argv[])
 {
     g_test_init (&argc, &argv, NULL);
 
     uzbl_commands_init ();
+    uzbl_variables_init ();
+    uzbl_io_init ();
 
     g_test_add_func ("/uzbl/commands/parse_simple", test_parse_simple);
     g_test_add_func ("/uzbl/commands/parse_quoted", test_parse_quoted);
     g_test_add_func ("/uzbl/commands/parse_extra_whitespace", test_parse_extra_whitespace);
     g_test_add_func ("/uzbl/commands/parse_escaped_at", test_parse_escaped_at);
+    g_test_add_func ("/uzbl/commands/chain", test_commands_chain);
 
     return g_test_run ();
 }
