f6b6def6369b067ce595d0616c133c4065ae3f96
David Keijser <keijser@gmail.com>
2017-02-17 18:29:20 +0100

    Add test of js command

--- tests/core-tests.c
+++ tests/core-tests.c
@@ -83,6 +83,34 @@ test_commands_chain ()
     g_main_loop_run (loop);
 }
 
+static void
+commands_js_cb (GObject      *source,
+                   GAsyncResult *res,
+                   gpointer      data)
+{
+    GMainLoop *loop = (GMainLoop*) data;
+    GError *err = NULL;
+    GString *r = uzbl_commands_run_finish (source, res, &err);
+
+    g_assert_null (err);
+    g_assert_cmpstr (r->str, ==, "foobar");
+
+    g_main_loop_quit (loop);
+}
+
+static void
+test_commands_js ()
+{
+    GMainLoop *loop = g_main_loop_new (NULL, FALSE);
+    GArray *argv = uzbl_commands_args_new ();
+    const UzblCommand *cmd = uzbl_commands_parse (
+        "js clean string '\"foo\" + \"bar\"'", argv);
+
+    uzbl_commands_run_async (cmd, argv, TRUE, commands_js_cb,
+                             (gpointer) loop);
+    g_main_loop_run (loop);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -97,6 +125,7 @@ main (int argc, char *argv[])
     g_test_add_func ("/uzbl/commands/parse_extra_whitespace", test_parse_extra_whitespace);
     g_test_add_func ("/uzbl/commands/parse_escaped_at", test_parse_escaped_at);
     g_test_add_func ("/uzbl/commands/chain", test_commands_chain);
+    g_test_add_func ("/uzbl/commands/js", test_commands_js);
 
     return g_test_run ();
 }
