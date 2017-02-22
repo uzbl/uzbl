9ecdda96621e61f24360c085ab2f33d897734c4d
Mateusz Poszwa <old4@o2.pl>
2017-02-22 19:06:46 +0100

    Lazily port auth.py to Python 3

--- examples/data/scripts/auth.py
+++ examples/data/scripts/auth.py
@@ -3,6 +3,13 @@
 from __future__ import print_function
 
 import os
+try:
+    import pygtkcompat
+except ImportError:
+    pygtkcompat = None
+if not (pygtkcompat is None):
+    pygtkcompat.enable()
+    pygtkcompat.enable_gtk()
 import gtk
 import sys
 import argparse
